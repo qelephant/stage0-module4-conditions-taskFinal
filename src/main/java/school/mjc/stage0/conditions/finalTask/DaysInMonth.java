81dfff706d27a690d4da9bb8515bf34546cc21eb
qyerke <70740293+qelephant@users.noreply.github.com>
2023-02-23 14:18:17 +0600

    Update DaysInMonth.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        int leapYear = year % 400 == 0 || year %4 == 0 && year % 100 != 0 ? 1 : 0;
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+               daysInMonth = 30;
+            } else if (month == 2) {
+                daysInMonth = (leapYear) ? 29 : 28;
+            else { 
+                daysInMonth = 31;
+            }
+        }
     }
 }
